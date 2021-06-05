5465641b21182375cfdc5a9de3789a0db54be2c9
sadat103 <1505103.sbf@ugrad.cse.buet.ac.bd>
2021-06-05 19:36:06 +0600

    navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,22 +10,38 @@ export class Header extends Component {
 
             <header>
             <div className = "menu" >
-            <img src = { Menu }
-            alt = ""
-            width = "20" / >
+            <img src = { Menu } alt = ""   width = "20" / >
 
             </div> 
             <div className = "logo" >
-            <h1>
-            <li > < Link to = "/" > Home </Link></li >
-            <li > < Link to = "/" > Home </Link></li >
-            <li > < Link to = "/" > Home </Link></li >
-            <li > < Link to = "/" > Home </Link></li >
-            <li > < Link to = "/" > Home </Link></li >
-            </h1>
+                <h1>
+                    <Link to="/">Nike</Link>
+                </h1>
+            </div>
+            <nav>
+                <ul>
+                     <li > < Link to = "/" > Home </Link></li >
+                     <li > < Link to = "/" > Product </Link></li >
+                     <li > < Link to = "/" > Contact </Link></li >
+                     <li > < Link to = "/" > About </Link></li >
+                     <li > < Link to = "/" > Login/Register </Link></li >
+                     <li className="close">
+                         <img src = {Close} alt = ""   width = "20" / >  
+                     </li>
 
 
-            </div>     
+                </ul>
+                <div className="nav-cart">
+                    <span>
+                        0
+                    </span>
+                    <Link to="/cart">
+                        <img src = {CartIcon} alt = ""   width = "20" / >
+                    </Link>
+                </div>
+                
+                
+              </nav>      
             </header >
 
         );
