917181f7c8eae99ff27df5880e57b70d3b4c41ef
sadat103 <1505103.sbf@ugrad.cse.buet.ac.bd>
2021-06-05 15:07:13 +0600

    initial

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom'
+import Header from './components/Header'
+import Section from './components/Section'
+
+class App extends React.Component {
+
+    render() {
+        return ( <
+            div className = "app" >
+            <
+            Router >
+            <
+            Header / >
+            <
+            Section / >
+            <
+            /Router> <
+            /div>
+
+
+        );
+
+    }
+
+
+
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom'
+
+export class Header extends Component {
+
+    render() {
+        return (
+
+            <
+            div >
+            Header
+
+            <
+            /div>
+
+
+        )
+    }
+}
+
+export default Header
